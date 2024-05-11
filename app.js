a569a6b91c03d91e54aedae9b2144b13fe9a05a5
nahashonkaroki <nahashon.karoki@gliffinglobaltech.com>
2024-05-11 21:22:34 +0300

    bot

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const productRoutes = require('./routes/product.router');
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '7000242911:AAFID_HmudqFWo3HQFo9B3W6nNGQdAnhHqA';
+const token = '';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
